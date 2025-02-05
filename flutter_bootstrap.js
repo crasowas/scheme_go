0772ce9f3841af6643a84eac5dbe67b92aa642e5
crasowas <crasowas@users.noreply.github.com>
2025-02-05 01:45:55 +0000

    Deploying to gh-pages from @ crasowas/scheme_go@f59c386a3d585baef60245373e6e067688c05e8f 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1805224339"
+    serviceWorkerVersion: "1726761312"
   }
 });
